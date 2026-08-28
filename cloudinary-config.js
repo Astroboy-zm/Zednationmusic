43080f941f5f269a9a7127b9410be4ee30f00dce
Zednationmusic <wynterjc05@gmail.com>
2026-08-28 10:38:08 +0200

    Add files via upload

--- cloudinary-config.js
+++ cloudinary-config.js
@@ -1,4 +1,5 @@
+// Replace these two values with your Cloudinary values.
 export const cloudinaryConfig = {
-  cloudName: "kkwd0sey",
-  uploadPreset: "Zednationmusic"
+  cloudName: "PASTE_YOUR_CLOUD_NAME",
+  uploadPreset: "PASTE_YOUR_UNSIGNED_UPLOAD_PRESET"
 };
